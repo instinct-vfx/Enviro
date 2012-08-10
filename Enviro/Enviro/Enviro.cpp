b16b7e3fa78a43124c1edd0b2cb9089448aa8e21
Thorsten Kaufmann <Thorsten.Kaufmann@mackevision.de>
2012-08-10 21:33:53 +0200

    * Initial skeleton for the Iop * Added comments to outline the todos

--- Enviro/Enviro/Enviro.cpp
+++ Enviro/Enviro/Enviro.cpp
@@ -1,10 +1,10 @@
-// TODO: Add disclaimer
+// TODO: Add license/disclaimer
 
 static const char* const CLASS = "Enviro";
 
 static const char* const HELP =
-"Enviro is a public exercise towards a reusable example for proper"
-"channel handling in Iops";
+"Enviro is a public exercise towards a reusable example for proper channel"
+"handling in Iops";
 
 #include "DDImage/Iop.h"
 #include "DDImage/Row.h"
@@ -15,10 +15,10 @@ using namespace DD::Image;
 
 class Enviro : public Iop
 {
-	// Parameters
-	double width, height;
+	// Parameters (Knobs)
 	Channel Normals[3];
 	Channel Position[3];
+	Channel Output[3];
 
 public:
 	Enviro(Node*);
@@ -36,89 +36,54 @@ public:
 
 Enviro::Enviro(Node* node) : Iop(node)
 {
-	width = 10;
-	height = 10;
+	//Initialize parameters
 	Normals[0] = Normals[1] = Normals[2] = Chan_Black;
 	Position[0] = Position[1] = Position[2] = Chan_Black;
+	Output[0] = Output[1] = Output[2] = Chan_Black;
 }
 
 void Enviro::knobs(Knob_Callback f)
 {
-	WH_knob(f, &width, "size");
+	//Knob to select the normals layer
 	Input_Channel_knob(f, Normals, 3, 0, "Normals");
-
+	//Knob to select the position layer
 	Input_Channel_knob(f, Position, 3, 0, "Position");
+	//Knob to select the output layer
+	Channel_knob(f, Output, 3, 0, "Output");
 
 }
 
-static Iop* Enviro_c(Node* node) { return new Enviro(node); }
-
-const Iop::Description Enviro::d(CLASS, "Filter/Enviro", Enviro_c);
-
 void Enviro::_validate(bool for_real)
 {
+	//TODO: Make sure we get the channels ans sizes produced from our inputs
+	//TODO: Get the cameradata here
 	copy_info();
 	set_out_channels(Mask_All);
 }
 
 void Enviro::_request(int x, int y, int r, int t, ChannelMask channels, int count)
 {
+	//TODO: Make sure we request all channels that are needed. This needs to include
+	//the channels that we are actually using (normals, position and the environment map)
 	input0().request(x, y, r, t, channels, count);
 }
 
 void Enviro::engine(int y, int x, int r, ChannelMask channels, Row& out)
 {
-	// Figure out a rectangle we want from the input:
-	int w = int(width);
-	int h = int(height);
-	int tx = x / w * w;
-	int tr = (r + w - 1) / w * w;
-	int ty = y / h * h;
-	int tt = ty + h;
 
-	// Lock an area into the cache:
-	// This should be an interest!!
-	Tile tile(input0(), tx, ty, tr, tt, channels);
-	// You must always check for aborted after creating a tile. If the
-	// operation was aborted, the tile contains bad data:
-	if (Op::aborted())
-		return;
+	//TODO: copy all channels we don't need or touch directly to output
 
-	for (int Y = ty; Y < tt; Y++) {
-		// Retrieve a row from the tile. This is much faster than random
-		// access of the tile and should be used if at all possible. See
-		// the documentation for Interest, Tile, at at() for other ways of
-		// getting the data.
-		Row in(tx, tr);
-		in.get(input0(), Y, tx, tr, channels);
-		// For each channel that is needed:
-		foreach (z, channels) {
-			// For each horizontal block:
-			for (int X = tx; X < tr; X += w) {
-				// add up all the incoming pixels:
-				float sum = 0;
-				for (int X1 = 0; X1 < w; X1++)
-					sum += in[z][X + X1];
-				// divide by the total size of a block:
-				sum /= (w * h);
-				// add it to the output pixels (for first row replace the output):
-				int outX = X;
-				int E = X + w;
-				if (outX < x)
-					outX = x;
-				if (E > r)
-					E = r;
-				float* TO = out.writable(z) + outX;
-				float* END = TO + (E - outX);
-				if (Y == ty) {
-					while (TO < END)
-						*TO++ = sum;
-				}
-				else {
-					while (TO < END)
-						*TO++ += sum;
-				}
-			}
-		}
-	}
+	//TODO: get input pixels from input0 channels in &normals and &position into
+	//local vars to make sure optimizer can identify them as static. This should
+	//be prepared to enable getting surrounding pixels for multisampling and later
+	//on glossy reflections.
+
+	//TODO: Lock the whole environment 
+
+	//TODO: Calculate coordinate in Environment. Get the color at the coordinate.
+	//TODO: Write resulting color into the layer set in &output
+
 }
+
+static Iop* Enviro_c(Node* node) { return new Enviro(node); }
+const Iop::Description Enviro::d(CLASS, "Filter/Enviro", Enviro_c);
